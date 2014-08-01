c491ebdd456e991bcdd7f47dd6ed668de1810576
jmscarpa <joao.scarpa@gmail.com>
2014-07-31 21:40:45 -0300

    Estrutura inicial gerada com yeoman + generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name confDesignaApp
+ * @description
+ * # confDesignaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('confDesignaApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name confDesignaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the confDesignaApp
+ */
+angular.module('confDesignaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
